9edf924a447f2b5d087e620ee50c64604f1e320c
Alex Hoenig <delimey@pm.me>
2024-02-15 12:04:50 +0000

    get app.py and jupyter notebook running

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from app_pages.page_1 import page_1_body
 from app_pages.page_2 import page_2_body
 from app_pages.page_3 import page_3_body
 
-app = MultiPage(app_name="ML App")  # Create an instance of the app
+app = MultiPage(app_name="Student AI")  # Create an instance of the app
 
 # Add your app pages here using .add_page()
 app.add_page("Page 1", page_1_body)
