ac183944d6c148a0a06340d41991de7eb959e03c
Alex Hoenig <delimey@pm.me>
2024-02-29 14:29:10 +0000

    setup data loading in streamlit dashboard

--- app_pages/page_3.py
+++ app_pages/page_3.py
@@ -1,6 +1,12 @@
 import streamlit as st
-import matplotlib.pyplot as plt
+import pandas as pd
+from src.data_management import load_original_data, load_cleaned_data
 
 def page_3_body():
     st.write("## DATASET")
-    st.write("### Exploratory Data Analysis")
+    st.write("### Original Dataset")
+    df_input = load_original_data()
+    st.write(df_input)
+
+    df_clean = load_cleaned_data()
+    st.write(df_clean)
