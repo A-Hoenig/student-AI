5984acb29463ac9b7b65caaf50bbdf648db39ab6
Alex Hoenig <delimey@pm.me>
2024-03-01 10:16:29 +0000

    add prediction engine and streamlit front end

--- app_pages/page_6.py
+++ app_pages/page_6.py
@@ -1,5 +1,109 @@
 import streamlit as st
+import pandas as pd
 import matplotlib.pyplot as plt
+from src.data_management import load_cleaned_data, load_pkl_file
+from src.ml.prediction_engine import (
+    maths, reading, writing)
+
 
 def page_6_body():
-    st.write("## Student Prediction")
+    st.write("## Prediction Engine")
+
+    st.info(
+        "This page uses the generated ML models to predict whether or not"
+        " a student might need help during their school year.\n\n"
+        "Select the specific student criteria and press calculate predictions"
+        " to see the result.\n"
+    )
+
+    PATH = "outputs/models/"
+    VERSION = "v1"
+
+    # load model files
+    TYPE = "math"
+    math_pipeline = load_pkl_file(
+        f"{PATH}{TYPE}/{VERSION}/{TYPE}-model.pkl")
+
+    math_label_map = load_pkl_file(
+        f"{PATH}{TYPE}/{VERSION}/{TYPE}-labels.pkl")
+
+    math_features = pd.read_csv(
+        f"{PATH}{TYPE}/{VERSION}/{TYPE}-train-features.csv"
+        ).columns.to_list()
+
+    TYPE = "reading"
+    reading_pipeline = load_pkl_file(
+        f"{PATH}{TYPE}/{VERSION}/{TYPE}-model.pkl")
+
+    reading_label_map = load_pkl_file(
+        f"{PATH}{TYPE}/{VERSION}/{TYPE}-labels.pkl")
+
+    reading_features = pd.read_csv(
+        f"{PATH}{TYPE}/{VERSION}/{TYPE}-train-features.csv"
+        ).columns.to_list()
+
+    TYPE = "writing"
+    writing_pipeline = load_pkl_file(
+        f"{PATH}{TYPE}/{VERSION}/{TYPE}-model.pkl")
+
+    writing_label_map = load_pkl_file(
+        f"{PATH}{TYPE}/{VERSION}/{TYPE}-labels.pkl")
+
+    writing_features = pd.read_csv(
+        f"{PATH}{TYPE}/{VERSION}/{TYPE}-train-features.csv"
+        ).columns.to_list()
+
+    # Acquire live data
+    input_features = AcquireInputs()
+
+    if st.button("Calculate Predictions"):
+        maths(
+            input_features, math_features, math_pipeline, math_label_map)
+
+        reading(
+            input_features, reading_features, reading_pipeline, reading_label_map)
+
+        writing(
+            input_features, writing_features, writing_pipeline, writing_label_map)
+
+def AcquireInputs():
+
+    df = load_cleaned_data()
+
+    column1, column2, column3, column4 = st.beta_columns(4)
+
+    input_features = pd.DataFrame([], index=[0])
+
+    with column1:
+        feature = 'LunchType'
+        streamlit_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+    input_features[feature] = streamlit_widget
+
+    with column2:
+        feature = 'EthnicGroup'
+        streamlit_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+    input_features[feature] = streamlit_widget
+
+    with column3:
+        feature = 'ParentEduc'
+        streamlit_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+    input_features[feature] = streamlit_widget
+
+    with column4:
+        feature = 'TestPrep'
+        streamlit_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+    input_features[feature] = streamlit_widget
+
+    return input_features
---
+++ src/ml/prediction_engine.py
@@ -0,0 +1,98 @@
+import streamlit as st
+
+
+def maths(
+    input_features,
+    math_features,
+    math_pipeline,
+    math_label_map):
+
+    # Subset feature variables
+    math_input_features = input_features.filter(math_features)
+
+    # Make prediction
+    math_prediction = math_pipeline.predict(math_input_features)
+    math_prediction_probability = math_pipeline.predict_proba(math_input_features)
+
+    # Display results
+    probability = math_prediction_probability[0, math_prediction][0]*100
+    math_label = math_label_map[math_prediction[0]]
+
+    if math_prediction != 1:
+        statement = (
+            "## Math Exam:\n"
+            "Prediction for student: "
+            f" **{math_label}** (Probability: {probability.round(2)}%)"
+        )
+    else:
+        statement = (
+            "## Math Exam:\n"
+            "Prediction for student: "
+            f" **{math_label}**"
+        )
+    st.write(statement)
+
+
+def reading(
+    input_features,
+    reading_features,
+    reading_pipeline,
+    reading_label_map):
+
+    # subset feature variables
+    reading_input_features = input_features.filter(reading_features)
+
+    # Make prediction
+    reading_prediction = reading_pipeline.predict(reading_input_features)
+    reading_prediction_probability = (
+        reading_pipeline.predict_proba(reading_input_features))
+
+    # display results
+    probability = reading_prediction_probability[0, reading_prediction][0]*100
+    reading_label = reading_label_map[reading_prediction[0]]
+
+    if reading_prediction != 1:
+        statement = (
+            "## Reading Exam:\n"
+            "Prediction for student: "
+            f" **{reading_label}** (Probability: {probability.round(2)}%)"
+        )
+    else:
+        statement = (
+            "## Reading Exam:\n"
+            "Prediction for student: "
+            f" **{reading_label}**"
+        )
+    st.write(statement)
+
+
+def writing(input_features,
+                    writing_features,
+                    writing_pipeline,
+                    writing_label_map):
+
+    # subset feature variables
+    writing_input_features = input_features.filter(writing_features)
+
+    # Make prediction
+    writing_prediction = writing_pipeline.predict(writing_input_features)
+    writing_prediction_probability = (
+        writing_pipeline.predict_proba(writing_input_features))
+
+    # display results
+    probability = writing_prediction_probability[0, writing_prediction][0]*100
+    writing_label = writing_label_map[writing_prediction[0]]
+
+    if writing_prediction != 1:
+        statement = (
+            "## Writing Exam:\n"
+            "Prediction for student: "
+            f" **{writing_label}** (Probability: {probability.round(2)}%)"
+        )
+    else:
+        statement = (
+            "## Writing Exam:\n"
+            "Prediction for student: "
+            f" **{writing_label}**"
+        )
+    st.write(statement)
