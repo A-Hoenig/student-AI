db504e963fd2f3b5eb57ccc2fc426c62ea0ddc05
Alex Hoenig <delimey@pm.me>
2024-03-05 15:08:06 +0000

    modify multipage class to allow grouping using buttons vs radio btn

--- app_pages/multipage.py
+++ app_pages/multipage.py
@@ -5,37 +5,39 @@ import streamlit as st
 class MultiPage:
 
     def __init__(self, app_name) -> None:
-        self.pages = []
+        self.pages = {}
         self.app_name = app_name
+        self.first_page_set = False
 
         st.set_page_config(
             page_title=self.app_name,
-            page_icon="🖥️")  # You may add an icon, to personalize your App
-        # check links below for additional icons reference
-        # https://docs.streamlit.io/en/stable/api.html#streamlit.set_page_config
-        # https://twemoji.maxcdn.com/2/test/preview.html
+            page_icon="🎓")
+        
+        # Remember the session state
+        if 'selected_page' not in st.session_state:
+            st.session_state.selected_page = None
 
-    def add_page(self, title, func, group=None) -> None:
-        self.pages.append({"title": title, "function": func, "group": group})
+    def add_page(self, title, func, group_name=None) -> None:
+        if group_name not in self.pages:
+            self.pages[group_name] = []
+        self.pages[group_name].append({"title": title, "function": func})
+        # Load first page by default
+        if not self.first_page_set:
+            st.session_state.selected_page = func
+            #remember page has been loaded
+            self.first_page_set = True
 
     def run(self):
         st.title(self.app_name)
-        # Initial = no group
-        current_group = None
-
-        for page in self.pages:
-            # Check for new group
-            if page['group'] != current_group:
-                # not the first group, add a separator
-                if current_group is not None:
-                    st.sidebar.markdown("---")
-                # Update current group to new group
-                current_group = page['group']
-                # Add group name as a header if you want
-                if current_group:
-                    st.sidebar.header(current_group)
-
-            # list the pages normally
-            if st.sidebar.button(page['title']):
-                page['function']()
-                break
+        
+        for group_name, pages in self.pages.items():
+            if group_name:
+                st.sidebar.header(group_name)
+            for page in pages:
+                button_key = f"button_{group_name}_{page['title']}"
+                if st.sidebar.button(page['title'], key=button_key):
+                    st.session_state.selected_page = page['function']
+        
+        # Run the selected page
+        if st.session_state.selected_page:
+            st.session_state.selected_page()
